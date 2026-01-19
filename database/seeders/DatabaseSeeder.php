fac5a8c91f6c7be837589a6a2522a645b3560614
Lambert Nsengimana <lambertnsengimana@Lamberts-MacBook-Pro.local>
2026-01-19 23:25:45 +0200

    feat: add profiles seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             UsersSeeder::class,
             SkillsSeeder::class,
             InterestsSeeder::class,
+            ProfilesSeeder::class
         ]);
     }
 }
